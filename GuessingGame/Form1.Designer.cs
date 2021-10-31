c73f4c6625b21ec913c119b6969ef83c3cc1ca29
Ppustota <z.vash22@gmail.com>
2021-10-31 14:04:29 +0400

    Add project files.

---
+++ GuessingGame/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace GuessingGame
+{
+    partial class GuessingGame
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.JapaneseButton = new System.Windows.Forms.Button();
+            this.ChineseButton = new System.Windows.Forms.Button();
+            this.KoreanButton = new System.Windows.Forms.Button();
+            this.ThaiButton = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureChange = new System.Windows.Forms.Timer(this.components);
+            this.pictureMove = new System.Windows.Forms.Timer(this.components);
+            this.pointsBox = new System.Windows.Forms.TextBox();
+            this.playAgainButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // JapaneseButton
+            // 
+            this.JapaneseButton.Location = new System.Drawing.Point(1, 1);
+            this.JapaneseButton.Name = "JapaneseButton";
+            this.JapaneseButton.Size = new System.Drawing.Size(75, 23);
+            this.JapaneseButton.TabIndex = 0;
+            this.JapaneseButton.Tag = "J";
+            this.JapaneseButton.Text = "Japanese";
+            this.JapaneseButton.UseVisualStyleBackColor = true;
+            this.JapaneseButton.Click += new System.EventHandler(this.JapaneseButton_Click);
+            // 
+            // ChineseButton
+            // 
+            this.ChineseButton.Location = new System.Drawing.Point(726, 1);
+            this.ChineseButton.Name = "ChineseButton";
+            this.ChineseButton.Size = new System.Drawing.Size(75, 23);
+            this.ChineseButton.TabIndex = 1;
+            this.ChineseButton.Tag = "C";
+            this.ChineseButton.Text = "Chinese";
+            this.ChineseButton.UseVisualStyleBackColor = true;
+            this.ChineseButton.Click += new System.EventHandler(this.ChineseButton_Click);
+            // 
+            // KoreanButton
+            // 
+            this.KoreanButton.Location = new System.Drawing.Point(1, 426);
+            this.KoreanButton.Name = "KoreanButton";
+            this.KoreanButton.Size = new System.Drawing.Size(75, 23);
+            this.KoreanButton.TabIndex = 2;
+            this.KoreanButton.Tag = "K";
+            this.KoreanButton.Text = "Korean";
+            this.KoreanButton.UseVisualStyleBackColor = true;
+            this.KoreanButton.Click += new System.EventHandler(this.KoreanButton_Click);
+            // 
+            // ThaiButton
+            // 
+            this.ThaiButton.Location = new System.Drawing.Point(726, 426);
+            this.ThaiButton.Name = "ThaiButton";
+            this.ThaiButton.Size = new System.Drawing.Size(75, 23);
+            this.ThaiButton.TabIndex = 3;
+            this.ThaiButton.Tag = "T";
+            this.ThaiButton.Text = "Thai";
+            this.ThaiButton.UseVisualStyleBackColor = true;
+            this.ThaiButton.Click += new System.EventHandler(this.ThaiButton_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(256, 256);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(244, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(276, 238);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureChange
+            // 
+            this.pictureChange.Enabled = true;
+            this.pictureChange.Interval = 3000;
+            this.pictureChange.Tick += new System.EventHandler(this.pictureChange_Tick);
+            // 
+            // pictureMove
+            // 
+            this.pictureMove.Enabled = true;
+            this.pictureMove.Tick += new System.EventHandler(this.pictureMove_Tick);
+            // 
+            // pointsBox
+            // 
+            this.pointsBox.Location = new System.Drawing.Point(296, 415);
+            this.pointsBox.Name = "pointsBox";
+            this.pointsBox.ReadOnly = true;
+            this.pointsBox.Size = new System.Drawing.Size(152, 23);
+            this.pointsBox.TabIndex = 5;
+            // 
+            // playAgainButton
+            // 
+            this.playAgainButton.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.playAgainButton.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.playAgainButton.Location = new System.Drawing.Point(278, 236);
+            this.playAgainButton.Name = "playAgainButton";
+            this.playAgainButton.Size = new System.Drawing.Size(191, 55);
+            this.playAgainButton.TabIndex = 6;
+            this.playAgainButton.Text = "Play Again";
+            this.playAgainButton.UseVisualStyleBackColor = true;
+            this.playAgainButton.Click += new System.EventHandler(this.playAgainButton_Click);
+            // 
+            // GuessingGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.playAgainButton);
+            this.Controls.Add(this.pointsBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ThaiButton);
+            this.Controls.Add(this.KoreanButton);
+            this.Controls.Add(this.ChineseButton);
+            this.Controls.Add(this.JapaneseButton);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GuessingGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GuessingGame";
+            this.Load += new System.EventHandler(this.GuessingGame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button JapaneseButton;
+        private System.Windows.Forms.Button ChineseButton;
+        private System.Windows.Forms.Button KoreanButton;
+        private System.Windows.Forms.Button ThaiButton;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer pictureChange;
+        private System.Windows.Forms.Timer pictureMove;
+        private System.Windows.Forms.TextBox pointsBox;
+        private System.Windows.Forms.Button playAgainButton;
+    }
+}
+
---
+++ GuessingGame/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GuessingGame
+{
+    public partial class GuessingGame : Form
+    {
+        #region Private members
+
+        private readonly string currentDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "\\images\\";
+        private int currenIndex = 0;
+        private int currentPoints = 0;
+        private bool buttonClicked;
+
+        #endregion
+
+        #region Default constructor
+        public GuessingGame()
+        {
+            InitializeComponent();
+            AddImagesToList();
+            NewGame(ref currentPoints);
+        }
+
+        #endregion
+
+        #region Custom methods
+
+        // Starts a new game and clears old results
+        private void NewGame(ref int points)
+        {
+            playAgainButton.Visible = false;
+            currenIndex = 0;
+            currentPoints = 0;
+            pointsBox.Text = String.Empty;
+            LoadNextPicture(ref currenIndex);
+        }
+
+        // Adds Pictures to image list and shuffles them
+        private void AddImagesToList()
+        {
+            string[] images = Directory.GetFiles(currentDirectory, "*.jpg");
+            Random random = new Random();
+            images = images.OrderBy(x => random.Next()).ToArray();
+            foreach (var image in images)
+            {
+                //key holds the info about nationality of the person on the picture
+                //which is dependent on the name of the image
+                string key = image.Replace(currentDirectory, "").Replace(".jpg", "");
+                imageList1.Images.Add(key, new Bitmap(image));
+            }
+        }
+   
+        // Loads next picture from image list or ends game if last picture was already loaded
+        private void LoadNextPicture(ref int index)
+        {
+            if (index < imageList1.Images.Count)
+            {
+                pictureBox1.Location = new System.Drawing.Point(244, 1);
+                pictureBox1.Image = imageList1.Images[index];
+                index++;
+            }
+            else
+            {
+                pointsBox.Text = $"You have {currentPoints} Points";
+                pictureBox1.Image = null;
+                playAgainButton.Visible = true;
+                return;
+            }
+        }
+        
+        // Manages point system
+        private void ResolvePoints(ref int index, Button button)
+        {
+            if (index == imageList1.Images.Count)
+            {       
+                    //compares images key and buttons tag to check if answer is correct
+                    //tags contain first letter of the country they represent, exmpl: 'J' for Japan
+                if (imageList1.Images.Keys[index - 1].Substring(0, 1).Equals(button.Tag.ToString(), StringComparison.Ordinal))
+                {
+                    currentPoints += 20;
+                    pointsBox.Text = $"You have {currentPoints} Points";
+                    LoadNextPicture(ref index);
+                }
+                else
+                {
+                    currentPoints -= 5;
+                    pointsBox.Text = $"You have {currentPoints} Points";
+                    LoadNextPicture(ref index);
+                }
+                pictureBox1.Image = null;
+                playAgainButton.Visible = true;
+                return;
+            }
+
+
+            if (imageList1.Images.Keys[index - 1].Substring(0, 1).Equals(button.Tag.ToString(), StringComparison.Ordinal))
+            {
+                currentPoints += 20;
+                pointsBox.Text = $"You have {currentPoints} Points";
+                LoadNextPicture(ref index);
+            }
+            else
+            {
+                currentPoints -= 5;
+                pointsBox.Text = $"You have {currentPoints} Points";
+                LoadNextPicture(ref index);
+            }
+        }
+
+        #endregion
+
+        #region Event handlers
+        private void GuessingGame_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.White;
+            playAgainButton.Visible = false;
+
+        }
+
+        private void pictureChange_Tick(object sender, EventArgs e)
+        {
+            if (buttonClicked)
+            {
+                buttonClicked = false;
+                return;
+            }
+            LoadNextPicture(ref currenIndex);
+        }
+
+        private void pictureMove_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new System.Drawing.Point(pictureBox1.Location.X, pictureBox1.Location.Y + 9); 
+        }
+
+        private void JapaneseButton_Click(object sender, EventArgs e)
+        {
+            buttonClicked = true;
+            ResolvePoints(ref currenIndex, JapaneseButton);
+        }
+        
+
+        private void ChineseButton_Click(object sender, EventArgs e)
+        {
+            buttonClicked = true;
+            ResolvePoints(ref currenIndex, ChineseButton);
+        }
+
+        private void KoreanButton_Click(object sender, EventArgs e)
+        {
+            buttonClicked = true;
+            ResolvePoints(ref currenIndex, KoreanButton);
+        }
+
+        private void ThaiButton_Click(object sender, EventArgs e)
+        {
+            buttonClicked = true;
+            ResolvePoints(ref currenIndex, ThaiButton);
+        }
+
+        
+        private void playAgainButton_Click(object sender, EventArgs e)
+        {
+            NewGame(ref currentPoints);
+        }
+        #endregion
+
+    }
+}
